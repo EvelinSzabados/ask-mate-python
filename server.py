495965a63b3383c70f4fb841f6c23e401f7d0c50
Evelin Szabados <szabadosevelin7@gmail.com>
2019-10-01 13:26:09 +0200

    Displaying table in questions_details

--- server.py
+++ server.py
@@ -11,17 +11,22 @@ def route_list():
 
 @app.route('/question')
 def route_question():
-    return render_template('question.html')
+    questions = connection.get_all_questions()
+    return render_template('question.html',questions=questions)
 
 
 @app.route('/new_question')
 def route_new_question():
     return render_template('new_question.html')
 
+@app.route('/answer')
+def route_answer():
+    return render_template('answer.html')
+
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=5000,
+        port=4000,
         debug=True,
     )
