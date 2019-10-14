d619f7884984f40822200a69975531b02a528363
Evelin Szabados <szabadosevelin7@gmail.com>
2019-10-14 14:27:08 +0200

    Rewriting list and new question to SQL

--- connection.py
+++ connection.py
@@ -1,45 +1,92 @@
-import csv
+import psycopg2
+import psycopg2.extras
 from datetime import datetime
 import os
 
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 
-def get_all_questions():
-    with open('sample_data/question.csv') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        questions = []
-        for line in csv_reader:
-            questions.append(dict(line))
+def get_connection_string():
+    # setup connection string
+    # to do this, please define these environment variables first
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
-    return questions
+    env_variables_defined = user_name and password and host and database_name
+
+    if env_variables_defined:
+        # this string describes all info for psycopg2 to connect to the database
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
-def get_all_answers():
-    with open('sample_data/answer.csv') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        answers = []
-        for line in csv_reader:
-            answers.append(dict(line))
+def open_database():
+    try:
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
 
-    return answers
 
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
 
-def add_new_answer(answers):
-    with open("sample_data/answer.csv", mode="w") as data_file:
-        fieldnames = ['id', 'submission_time', 'vote_number', 'question_id', 'message']
-        data_writer = csv.DictWriter(data_file, delimiter=',', fieldnames=fieldnames)
-        data_writer.writeheader()
+        return ret_value
 
-        for data in answers:
-            data_writer.writerow(data)
+    return wrapper
 
-def add_new_question(questions):
-
-    with open("sample_data/question.csv", mode="w") as data_file:
-        fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-        data_writer = csv.DictWriter(data_file, delimiter=',', fieldnames=fieldnames)
-        data_writer.writeheader()
-
-        for data in questions:
-            data_writer.writerow(data)
+# def get_all_questions():
+#     with open('sample_data/question.csv') as csv_file:
+#         csv_reader = csv.DictReader(csv_file, delimiter=',')
+#         questions = []
+#         for line in csv_reader:
+#             questions.append(dict(line))
+#
+#     return questions
+#
+#
+# def get_all_answers():
+#     with open('sample_data/answer.csv') as csv_file:
+#         csv_reader = csv.DictReader(csv_file, delimiter=',')
+#         answers = []
+#         for line in csv_reader:
+#             answers.append(dict(line))
+#
+#     return answers
+#
+#
+# def add_new_answer(answers):
+#     with open("sample_data/answer.csv", mode="w") as data_file:
+#         fieldnames = ['id', 'submission_time', 'vote_number', 'question_id', 'message']
+#         data_writer = csv.DictWriter(data_file, delimiter=',', fieldnames=fieldnames)
+#         data_writer.writeheader()
+#
+#         for data in answers:
+#             data_writer.writerow(data)
+#
+# def add_new_question(questions):
+#
+#     with open("sample_data/question.csv", mode="w") as data_file:
+#         fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+#         data_writer = csv.DictWriter(data_file, delimiter=',', fieldnames=fieldnames)
+#         data_writer.writeheader()
+#
+#         for data in questions:
+#             data_writer.writerow(data)
--- data_manager.py
+++ data_manager.py
@@ -4,71 +4,106 @@ import time
 from datetime import datetime
 
 
-def convert_questions():
+@connection.connection_handler
+def get_questions_sql(cursor):
+    cursor.execute("SELECT * FROM question;")
+    all_questions = cursor.fetchall()
 
-    converted_questions = connection.get_all_questions()
-    for line in converted_questions:
-        for key, value in line.items():
-            if key == "submission_time":
-                converted_time = datetime.fromtimestamp(int(value))
-                line[key] = str(converted_time)
-    converted_questions.sort(key=lambda x: x['submission_time'], reverse=True)
-    return converted_questions
+    return all_questions
 
 
-def convert_answers():
+@connection.connection_handler
+def add_new_question(cursor, new_data):
+    cursor.execute(
+        "INSERT INTO question VALUES (DEFAULT, %(submission_time)s,%(view_number)s,%(vote_number)s, %(title)s ,%(message)s,NULL) RETURNING id;",
+        {'submission_time': new_data["submission_time"],
+         'view_number': new_data["view_number"],
+         'vote_number': new_data["vote_number"],
+         'title': new_data["title"],
+         'message': new_data["message"]})
+    question_id = cursor.fetchall()
+    return question_id
 
-    converted_answers = connection.get_all_answers()
-    for line in converted_answers:
-        for key, value in line.items():
-            if key == "submission_time":
-                converted_time = datetime.fromtimestamp(int(value))
-                line[key] = str(converted_time)
-    converted_answers.sort(key=lambda x: x['submission_time'], reverse=True)
-    return converted_answers
 
-def get_next_id():
-    existing_data = connection.get_all_questions()
-    if len(existing_data) == 0:
-        return '1'
+# def convert_questions():
+#
+#     converted_questions = connection.get_all_questions()
+#     for line in converted_questions:
+#         for key, value in line.items():
+#             if key == "submission_time":
+#                 converted_time = datetime.fromtimestamp(int(value))
+#                 line[key] = str(converted_time)
+#     converted_questions.sort(key=lambda x: x['submission_time'], reverse=True)
+#     return converted_questions
+#
+#
+# def convert_answers():
+#
+#     converted_answers = connection.get_all_answers()
+#     for line in converted_answers:
+#         for key, value in line.items():
+#             if key == "submission_time":
+#                 converted_time = datetime.fromtimestamp(int(value))
+#                 line[key] = str(converted_time)
+#     converted_answers.sort(key=lambda x: x['submission_time'], reverse=True)
+#     return converted_answers
+#
+# def get_next_id():
+#     existing_data = connection.get_all_questions()
+#     if len(existing_data) == 0:
+#         return '1'
+#
+#     return str(int(existing_data[-1]['id']) + 1)
 
-    return str(int(existing_data[-1]['id']) + 1)
 
 def current_submission_time():
-
     submission_time = int(time.time())
+    converted_time = datetime.fromtimestamp(int(submission_time))
+    timestamp = str(converted_time)
+    return timestamp
 
-    return submission_time
 
-def get_actual_question(question_id):
-    questions = convert_questions()
-    actual_question = []
-    for line in questions:
-        if line["id"] == question_id:
-            actual_question.append(dict(line))
+@connection.connection_handler
+def get_actual_question(cursor, id):
+    cursor.execute("SELECT * FROM question WHERE id=%(id)s;",
+                   {'id': id})
+    actual_question = cursor.fetchall()
     return actual_question
 
-def get_actual_answer(question_id):
-    answers = convert_answers()
-    actual_answers = []
-    for answer in answers:
-        if answer["question_id"] == question_id:
-            actual_answers.append(dict(answer))
-    return actual_answers
 
-
-def delete_question(question_id):
-    questions = connection.get_all_questions()
-    for question in questions:
-        if question["id"] == question_id:
-            questions.remove(question)
-
-    return questions
-
-
-def delete_answer(actual_id):
-    answers = connection.get_all_answers()
-    for answer in answers:
-        if answer["id"] == actual_id:
-            answers.remove(answer)
-    return answers
+#     questions = convert_questions()
+#     actual_question = []
+#     for line in questions:
+#         if line["id"] == question_id:
+#             actual_question.append(dict(line))
+#     return actual_question
+#
+@connection.connection_handler
+def get_actual_answer(cursor, id):
+    cursor.execute("SELECT * FROM answer WHERE id=%(id)s;",
+                   {'id': id})
+    actual_answer = cursor.fetchall()
+    return actual_answer
+#     answers = convert_answers()
+#     actual_answers = []
+#     for answer in answers:
+#         if answer["question_id"] == question_id:
+#             actual_answers.append(dict(answer))
+#     return actual_answers
+#
+#
+# def delete_question(question_id):
+#     questions = connection.get_all_questions()
+#     for question in questions:
+#         if question["id"] == question_id:
+#             questions.remove(question)
+#
+#     return questions
+#
+#
+# def delete_answer(actual_id):
+#     answers = connection.get_all_answers()
+#     for answer in answers:
+#         if answer["id"] == actual_id:
+#             answers.remove(answer)
+#     return answers
