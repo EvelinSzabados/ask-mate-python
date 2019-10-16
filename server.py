457cfcde78e99fbac12185ba78447eea2b40bbba
VaradiPeterRoland <varadi.peter.roland95@gmail.com>
2019-10-16 11:32:54 +0200

    comment to question now works fine

--- server.py
+++ server.py
@@ -98,19 +98,32 @@ def search():
 
 
 @app.route('/question/<question_id>/new-comment', methods=['GET', 'POST'])
-def route_comment(question_id):
+def route_comment_to_question(question_id):
     if request.method == 'POST':
         new_comment_data = {
             "question_id": int(question_id),
+            "answer_id":None,
             "message": request.form.get("message"),
             "submission_time": data_manager.current_submission_time(),
         }
-        data_manager.add_new_answer(new_comment_data)
-
+        data_manager.add_new_comment(new_comment_data)
         return redirect(url_for('route_question', question_id=question_id))
 
-    return render_template('comment.html', form_url=url_for('route_comment', question_id=question_id))
+    return render_template('comment.html', form_url=url_for('route_comment_to_question', question_id=question_id))
 
+@app.route('/answer/<answer_id>/new-comment', methods=['GET', 'POST'])
+def route_comment_to_answer(answer_id):
+    if request.method == 'POST':
+        new_comment_data = {
+            "answer_id": int(answer_id),
+            "message": request.form.get("message"),
+            "submission_time": data_manager.current_submission_time(),
+        }
+        data_manager.add_new_comment(new_comment_data)
+        question_id = data_manager.get_actual_question_by_answer_id(answer_id)[0]['question_id']
+        return redirect(url_for('route_question', question_id=question_id))
+
+    return render_template('comment.html', form_url=url_for('route_comment_to_answer', answer_id=answer_id))
 
 if __name__ == '__main__':
     app.run(
